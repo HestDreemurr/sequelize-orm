da56804d3dc0eb868a21c4fb0ed6b71c32567ddc
Hest <hestdatabase@gmail.com>
2025-07-10 18:29:50 -0300

    Operações DELETE e UPUPDA

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { sequelize, Users } from "#sequelize";
+import { Op } from "sequelize";
 import { randomUUID } from "crypto";
 
 sequelize.sync();
@@ -13,20 +14,64 @@ console.log("Sequelize sincronizado com o Banco de Dados.");
 async function saveUser() {
   const user = await Users.create({
     id: randomUUID(),
-    name: "Fulano",
-    email: "fulano@gmail.com"
+    name: "Jonh Miller",
+    email: "jonhmiller@gmail.com"
   });
   
-  console.log(`Usuário criado: ${user.name}\n`);
+  console.log(`Usuário criado: ${JSON.stringify(user, null, 2)}\n`);
 }
 // saveUser();
 
 // Procurar usuários
 async function findUsers() {
   const data = await Users.findAll({
-    attributes: ["name", "email"]
+    attributes: ["name", "email"],
+    where: {
+      name: {
+        [Op.iLike]: "%Jonh%"
+      }
+    }
   });
   console.log(`Usuários no banco: ${JSON.stringify(data, null, 2)}`);
 }
 
-findUsers();
+async function findUser(email: string) {
+  const data = await Users.findOne({
+    where: {
+      email
+    }
+  });
+  
+  console.log(`Usuário encontrado: ${JSON.stringify(data, null, 2)}`);
+}
+// findUser("hest@gmail.com");
+// findUsers();
+
+// Atualizar usuários
+async function updateUsers() {
+  await Users.update(
+    { name: "Hest lindo" },
+    {
+      where: {
+        name: "Hest"
+      }
+    }
+  );
+  
+  console.log("Usuários atualizados.");
+}
+// updateUsers();
+
+// Deletar usuários
+async function deleteUsers() {
+  await Users.destroy({
+    where: {
+      name: {
+        [Op.iLike]: "%Jonh%"
+      }
+    }
+  });
+  
+  console.log("Usuários deletados.");
+}
+// deleteUsers();
